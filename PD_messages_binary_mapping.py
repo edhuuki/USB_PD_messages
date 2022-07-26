10c6244d9be4978f6605fa6bedd6079f35bdd6fa
Erik Huuki <79381333+edhuuki@users.noreply.github.com>
2022-07-25 19:48:24 -0500

    updated mappings to match bit indexes in spec sheet

--- PD_messages_binary_mapping.py
+++ PD_messages_binary_mapping.py
@@ -53,8 +53,7 @@ k_codes5b = {
 	'00010':'Reserved',
 	'00011':'Reserved',
 	'00100':'Reserved',
-	'00101':'Reserved',
-	
+	'00101':'Reserved'
 	}
 k_codes4b = {
 	'0000':'0',
@@ -74,24 +73,31 @@ k_codes4b = {
 	'1110':'E',
 	'1111':'F'
 	}
-
-fiveBit_4Bit_decoder = {
-	'11110':'0000',
-	'01001':'0001',
-	'10100':'0010',
-	'10101':'0011',
-	'01000':'0100',
-	'01011':'0101',
-	'01110':'0110',
-	'01111':'0111',
-	'10010':'1000',
-	'10011':'1001',
-	'10110':'1010',
-	'10111':'1011',
-	'11010':'1100',
-	'11011':'1101',
-	'11100':'1110',
-	'11101':'1111'
+# Table 5-1 pg 95
+# Values are reversed so that indexes matches the bit encoding 
+fiveBit_fourBit_decoder = {
+	'01111':'0000',		# 0
+	'10010':'1000',		# 1
+	'00101':'0100',		# 2
+	'10101':'1100',		# 3
+	'01010':'0010',		# 4
+	'11010':'1010',		# 5
+	'01110':'0110',		# 6
+	'11110':'1110',		# 7
+	'01001':'0001',		# 8
+	'11001':'1001',		# 9
+	'01101':'0101',		# A
+	'11101':'1101',		# B
+	'01011':'0011',		# C
+	'11011':'1011',		# D
+	'00111':'0111',		# E
+	'10111':'1111',		# F
+	'00011':'SYNC1',
+	'10001':'SYNC2',
+	'11100':'RST1',
+	'10011':'RST2',
+	'10110':'EOP',		# End of packet 
+	'01100':'SYNC3'
 	}
 
 
@@ -100,35 +106,68 @@ fiveBit_4Bit_decoder = {
 # Key value pairs are 'bit stream':'control message'
 control_messages = {
 	'00000':'Reserved',
-	'00001':'GoodCRC',
-	'00010':'GotoMin',
-	'00011':'Accept',
+	'10000':'GoodCRC',
+	'01000':'GotoMin',
+	'11000':'Accept',
 	'00100':'Reject',
-	'00101':'Ping',
-	'00110':'PS_RDY',
-	'00111':'Get_Source_Cap',
-	'01000':'Get_Sink_Cap',
-	'01001':'DR_Swap',
+	'10100':'Ping',
+	'01100':'PS_RDY',
+	'11100':'Get_Source_Cap',
+	'00010':'Get_Sink_Cap',
+	'10010':'DR_Swap',
 	'01010':'PR_Swap',
-	'01011':'Vconn_Swap',
-	'01100':'Wait',
-	'01101':'Soft_reset',
+	'11010':'Vconn_Swap',
+	'00110':'Wait',
+	'10110':'Soft_reset',
 	'01110':'Data_Reset',
-	'01111':'Data_Reset_Complete',
-	'10000':'Not_Supported',
+	'11110':'Data_Reset_Complete',
+	'00001':'Not_Supported',
 	'10001':'Get_source_Cap_extended',
-	'10010':'Get_status',
-	'10011':'FR_Swap',
-	'10100':'Get_PPS_Status',
+	'01001':'Get_status',
+	'11001':'FR_Swap',
+	'00101':'Get_PPS_Status',
 	'10101':'Get_Country_Codes',
-	'10110':'Get_Sink_Cap_extended',
-	'10111':'Get_Source_info',
-	'11000':'Get_Revision'
+	'01101':'Get_Sink_Cap_extended',
+	'11101':'Get_Source_info',
+	'00011':'Get_Revision'
 	}
 
+# for i in range(16,32):		# Instantiates all the bits that map to 'Reserved' in control messages
+# 	control_messages[str(bin(i))[3:]] = 'Reserved'
 
-for i in range(16,32):		# Instantiates all the bits that map to 'Reserved' in control messages
-	control_messages[str(bin(i))[2:]] = 'Reserved'
+
+
+data_messages = {
+	'00000':'Reserved',
+	'00001':'Source_Capabilities',
+	'00010':'Request',
+	'00011':'BIST',						# Built in self test
+	'00100':'Sink_Capabilities',
+	'00101':'Battery_Status',
+	'00110':'Alert',
+	'00111':'Get_Country_Info',
+	'01000':'Enter_USB',
+	'01001':'EPR_Request',
+	'01010':'EPR_Mode',
+	'01011':'Source_Info',
+	'01100':'Revision',
+	'01101':'Reserved',
+	'01110':'Reserved',
+	'01111':'Vendor_Defined',
+	'10000':'Reserved',
+	'10001':'Reserved',
+	'10010':'Reserved',
+	'10011':'Reserved',
+	'10100':'Reserved',
+	'10101':'Reserved',
+	'10110':'Reserved',
+	'10111':'Reserved',
+	'11000':'Reserved'
+}
+for i in range(16,32):
+	data_messages[bin(i)[2:]] = 'Reserved'
+
+
 
 preamble = ''.join(['01']*32) # preamble starts with 64 alternating bits starting with 0
 
